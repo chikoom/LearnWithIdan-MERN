12ebb04ee22ba20b9dbb9d7e2d3e61f2633032b6
Chikoom <callforcopy@gmail.com>
2020-04-20 13:34:56 +0300

    Initiated react file, prop types and default message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import propTypes from 'prop-types';
 
 
+const App = (props) => {
+  return (
+    <div>
+      <h2 className="text-container">
+        Hello React Component
+      </h2>
+      <p>{props.headerMessage}</p>
+    </div>
+  );
+};
+
+App.propTypes = {
+  headerMessage: propTypes.string.isRequired
+};
+
+App.defaultProps = {
+  headerMessage: 'Default Message!'
+};
 
 ReactDOM.render(
-  React.createElement('h2', null, 'React Hello'),
+  <App headerMessage="A props message" />,
   document.getElementById('root')
 );
