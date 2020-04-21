503e8c55018a88e7551dc8e9ee6758a3a7aaab6a
Chikoom <callforcopy@gmail.com>
2020-04-21 13:41:41 +0300

    Changed react render to react hydrate, for handling the server-client data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <App initialBriefs={window.initialData.briefs} />,
   document.getElementById('root')
 );
