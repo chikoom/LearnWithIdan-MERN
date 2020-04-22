42c0986768190e4096352f0e0e6f6271c562271c
Chikoom <callforcopy@gmail.com>
2020-04-22 15:19:35 +0300

    connected to mongoDB for recieving real data

--- api/index.js
+++ api/index.js
@@ -1,20 +1,40 @@
 import express from 'express';
-import data from '../src/testData.json';
+import { MongoClient } from 'mongodb';
+import { strict as assert } from 'assert';
+import config from '../config';
+
+
+let mdb;
+MongoClient.connect(config.mongodbUri, { useUnifiedTopology: true, useNewUrlParser: true,}, (err, client) => {
+  assert.equal(null, err);
+  mdb = client.db(config.mongodbDB);
+});
 
 const router = express.Router();
-const briefs = data.briefs.reduce((obj, brief) => {
-  obj[brief.id] = brief;
-  return obj;
-},{});
+
 
 router.get('/briefs', (req, res) => {
-  res.send({briefs});
+  let briefs = {};
+  mdb.collection('briefs').find({})
+    .project({
+      id:1,
+      lessonName:1,
+      briefName:1
+    })
+    .each((err, brief) => {
+      assert.equal(null, err);
+      if(!brief){
+        res.send({briefs});
+        return;
+      }
+      briefs[brief.id] = brief;
+    });
 });
 
 router.get('/briefs/:briefId', (req, res) => {
-  let brief = briefs[req.params.briefId];
-  brief.description = 'This is the brief description';
-  res.send(brief);
+  mdb.collection('briefs').findOne({ id: Number(req.params.briefId) })
+    .then(brief => res.send(brief))
+    .catch(console.error);
 });
 
 export default router;
--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 const env  = process.env;
 
+export const noneEnv = env.NODE_ENV || 'development';
+
 export default {
+  mongodbUri: 'mongodb://localhost:27017',
+  mongodbDB: 'learningwidan',
   port: env.PORT || 8080,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
